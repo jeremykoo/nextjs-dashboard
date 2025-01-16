0b4e6f52e6b8c38dadfd285c2797d3e81f58424b
Jeremy Koo <38170465+jeremykoo@users.noreply.github.com>
2025-01-15 17:08:25 -0800

    added search

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // https://nextjs.org/docs/messages/ppr-preview
+  // experimental: {
+  //   ppr: 'incremental',
+  // },
 };
 
 export default nextConfig;
